88890a38fa7656f09884366b00ecec473b02d417
Ankush-Phulia <ankush.phulia22@gmail.com>
2016-02-20 14:49:21 +0530

    update

--- app/src/main/java/com/ribieroboys/moodleplus/Profile.java
+++ app/src/main/java/com/ribieroboys/moodleplus/Profile.java
@@ -1,13 +1,18 @@
 package com.ribieroboys.moodleplus;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
+
+    String user;
+    String pass;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +21,28 @@ public class Profile extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Intent data=getIntent();
+        user=data.getStringExtra("user");
+        pass=data.getStringExtra("pass");
+        TextView username= (TextView) findViewById(R.id.textView11);
+        username.setText(user);
+        TextView fullname= (TextView) findViewById(R.id.textView13);
+        fullname.setText(" ");
+        back=(Button) findViewById(R.id.button);
+
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                end();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public void onBackPressed() {
+    }
+
+    public void end(){
+        this.finish();
+    }
 }
