bf76d80cb11c5498df89db67dd769ca23aada015
Nitin Rathor <nitinworld.2050@gmail.com>
2016-02-24 11:08:04 +0530

    Assignment fragment populated

--- app/src/main/java/com/ribieroboys/moodleplus/FragmentChild1.java
+++ app/src/main/java/com/ribieroboys/moodleplus/FragmentChild1.java
@@ -6,13 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
-
 import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class FragmentChild1 extends Fragment {
     TextView assignView;
     JSONArray assignJSON;
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -28,14 +37,78 @@ public class FragmentChild1 extends Fragment {
 
         try {
             assignJSON = new JSONArray(getArguments().getString("Assignments"));
-            //do something here
 
+            expListView = (ExpandableListView) view.findViewById(R.id.expandableListView);
+
+            listAdapter = new ExpandableListAdapter(this.getContext(), listDataHeader, listDataChild);
+            expListView.setAdapter(listAdapter);
+            prepareListData();
         }
         catch (Exception e) {
             e.printStackTrace();
         }
 
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+
+                return true;
+            }
+        });
+
+
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                switch (groupPosition) {
+
+                    default:
+                        return true;
+                }
+
+            }
+        });
+
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+            }
+        });
+
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+            }
+        });
+
+
         return view;
+    }
+
+    private void prepareListData() {
+        try {
+            listDataHeader = new ArrayList<String>();
+            listDataChild = new HashMap<String, List<String>>();
+
+            for (int assignNo=0; assignNo < assignJSON.length(); assignNo++) {
+                JSONObject assignment = (JSONObject) assignJSON.get(assignNo);
+                String header = assignment.getString("name");
+                listDataHeader.add(header);
+
+                List<String> childData = new ArrayList<>();
+                childData.add("Deadline: " + assignment.getString("deadline") + "\n");
+                childData.add(assignment.getString("description"));
+                listDataChild.put(listDataHeader.get(assignNo), childData);
+            }
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- app/src/main/java/com/ribieroboys/moodleplus/FragmentChild3.java
+++ app/src/main/java/com/ribieroboys/moodleplus/FragmentChild3.java
@@ -6,14 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class FragmentChild3 extends Fragment {
     TextView threadsView;
     JSONArray threadsJSON;
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -29,15 +38,78 @@ public class FragmentChild3 extends Fragment {
 
         try {
             threadsJSON = new JSONArray(getArguments().getString("Threads"));
-            //do something here
+
+            expListView = (ExpandableListView) view.findViewById(R.id.expandableListView);
+
+            listAdapter = new ExpandableListAdapter(this.getContext(), listDataHeader, listDataChild);
+            expListView.setAdapter(listAdapter);
+            prepareListData();
 
         }
         catch (Exception e) {
             e.printStackTrace();
         }
 
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+
+                return true;
+            }
+        });
+
+
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                switch (groupPosition) {
+
+                    default:
+                        return true;
+                }
+
+            }
+        });
+
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+            }
+        });
+
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+            }
+        });
+
+
         return view;
     }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        listDataHeader.add("Overview");
+        listDataHeader.add("Notifications");
+        listDataHeader.add("Grades");
+        listDataHeader.add("Courses");
+
+        List<String> Overview = new ArrayList<String>();
+        List<String> Notifications = new ArrayList<String>();
+        List<String> Grades = new ArrayList<String>();
+        List<String> Courses = new ArrayList<String>();
+        listDataChild.put(listDataHeader.get(0), Overview);
+        listDataChild.put(listDataHeader.get(1), Notifications);
+        listDataChild.put(listDataHeader.get(2), Grades);
+        listDataChild.put(listDataHeader.get(3), Courses);
+    }
+
 
 
 }
