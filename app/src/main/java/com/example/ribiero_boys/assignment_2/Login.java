59336b683fdd4743e5ad083e3a1e227eb03be8fb
Ankush-Phulia <ankush.phulia22@gmail.com>
2016-03-27 15:14:54 +0530

    UI part I

---
+++ app/src/main/java/com/example/ribiero_boys/assignment_2/Login.java
@@ -0,0 +1,113 @@
+package com.example.ribiero_boys.assignment_2;
+
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class Login extends Fragment implements View.OnClickListener {
+
+    String username,password;
+    Button log;
+    EditText editTextUsername,editTextPassword;
+    CheckBox saveLoginCheckBox;
+    SharedPreferences loginPreferences;
+    SharedPreferences.Editor loginPrefsEditor;
+    boolean saveLogin;
+    boolean savePass;
+
+    public Login() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_login, container, false);
+        //get the ids of all elements
+        getIDs(view);
+        //for recalling previosly saved user/pass
+        loginPreferences = getActivity().getSharedPreferences("loginPrefs", 0);
+        loginPrefsEditor = loginPreferences.edit();
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+        savePass = loginPreferences.getBoolean("savePass", false);
+        if (saveLogin == true) {
+            editTextUsername.setText(loginPreferences.getString("username", ""));
+            saveLoginCheckBox.setChecked(true);
+        }
+        if (savePass == true) {
+            editTextPassword.setText(loginPreferences.getString("password", ""));
+        }
+
+        log.setOnClickListener(this);
+
+        return view;
+    }
+
+    private void getIDs(View V) {
+        // fetch all UI elements
+        log = (Button) V.findViewById(R.id.buttonLogin);
+        editTextUsername = (EditText) V.findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) V.findViewById(R.id.editTextPassword);
+        saveLoginCheckBox = (CheckBox) V.findViewById(R.id.saveLoginCheckBox);
+    }
+
+    private void endActivityAndStartNew(Intent i) {
+        startActivity(i);
+        getActivity().finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==log){
+
+            final Intent intent=new Intent(getContext(),Main.class);
+            username = editTextUsername.getText().toString();
+            password = editTextPassword.getText().toString();
+
+            if (saveLoginCheckBox.isChecked()) {
+                //save the username in shared prefs
+                loginPrefsEditor.putBoolean("saveLogin", true);
+                loginPrefsEditor.putString("username", username);
+
+                new AlertDialog.Builder(this.getContext(), AlertDialog.THEME_HOLO_LIGHT)
+                        .setTitle("Remember Password")
+                        .setMessage("Should the Password be saved?")
+                        .setPositiveButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                loginPrefsEditor.putBoolean("savePass", false);
+                                loginPrefsEditor.commit();
+                                endActivityAndStartNew(intent);
+                            }
+                        })
+                        .setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                loginPrefsEditor.putBoolean("savePass", true);
+                                loginPrefsEditor.putString("password", password);
+                                loginPrefsEditor.commit();
+                                endActivityAndStartNew(intent);
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            } else {
+                loginPrefsEditor.clear();
+                loginPrefsEditor.commit();
+                endActivityAndStartNew(intent);
+            }
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/ribiero_boys/assignment_2/default_start.java
@@ -0,0 +1,58 @@
+package com.example.ribiero_boys.assignment_2;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class default_start extends Fragment {
+
+
+    public default_start() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_default_start, container, false);
+        //fetch the buttons
+        Button gotologin=(Button)view.findViewById(R.id.login);
+        Button gotosignup=(Button)view.findViewById(R.id.sign_up);
+
+        gotologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login LoginFragment = new Login();
+                FragmentTransaction change=getFragmentManager().beginTransaction();
+                change.setCustomAnimations(R.anim.slide_in_right,R.anim.slide_out_left,R.anim.slide_in_left,R.anim.slide_out_right);
+                change.replace(R.id.start_fragments, LoginFragment).addToBackStack("default");
+                change.commit();
+        }
+        });
+
+        gotosignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Sign_up SignFragment = new Sign_up();
+                FragmentTransaction change=getFragmentManager().beginTransaction();
+                change.setCustomAnimations(R.anim.slide_in_left,R.anim.slide_out_right,R.anim.slide_in_right,R.anim.slide_out_left);
+                change.replace(R.id.start_fragments, SignFragment).addToBackStack("default2");
+                change.commit();
+            }
+        });
+
+        return view;
+    }
+
+
+}
